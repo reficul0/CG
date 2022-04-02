4878fee148de69d9b5ae0bb1183d4e77d391e368
reficul0 <cheshirwr@mail.ru>
2022-04-02 18:24:51 +0700

    OpenGL lab with all 2D primitives.

--- OpenGL/OpenGLForm.Designer.cs
+++ OpenGL/OpenGLForm.Designer.cs
@@ -29,7 +29,25 @@ namespace OpenGL
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.mHolst = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pointsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linesP = new System.Windows.Forms.ToolStripMenuItem();
+            this.lines = new System.Windows.Forms.ToolStripMenuItem();
+            this.linesStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.linesLoop = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesP = new System.Windows.Forms.ToolStripMenuItem();
+            this.triangles = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesFan = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.quads = new System.Windows.Forms.ToolStripMenuItem();
+            this.quadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quadStripToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.polygon = new System.Windows.Forms.ToolStripMenuItem();
+            this.task = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mHolst
@@ -45,21 +63,147 @@ namespace OpenGL
             this.mHolst.BackColor = System.Drawing.Color.Black;
             this.mHolst.ColorBits = ((byte)(32));
             this.mHolst.DepthBits = ((byte)(16));
-            this.mHolst.Location = new System.Drawing.Point(0, -1);
+            this.mHolst.Location = new System.Drawing.Point(-1, -2);
             this.mHolst.Name = "mHolst";
-            this.mHolst.Size = new System.Drawing.Size(1055, 718);
+            this.mHolst.Size = new System.Drawing.Size(1056, 719);
             this.mHolst.StencilBits = ((byte)(0));
             this.mHolst.TabIndex = 0;
-            this.mHolst.Paint += new System.Windows.Forms.PaintEventHandler(this.mHolst_Paint);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pointsToolStripMenuItem,
+            this.linesP,
+            this.trianglesP,
+            this.quads,
+            this.polygon,
+            this.task});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 158);
+            // 
+            // pointsToolStripMenuItem
+            // 
+            this.pointsToolStripMenuItem.Name = "pointsToolStripMenuItem";
+            this.pointsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pointsToolStripMenuItem.Text = "Point";
+            this.pointsToolStripMenuItem.Click += new System.EventHandler(this.pointsToolStripMenuItem_Click);
+            // 
+            // linesP
+            // 
+            this.linesP.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lines,
+            this.linesStrip,
+            this.linesLoop});
+            this.linesP.Name = "linesP";
+            this.linesP.Size = new System.Drawing.Size(180, 22);
+            this.linesP.Text = "Lines";
+            // 
+            // lines
+            // 
+            this.lines.Name = "lines";
+            this.lines.Size = new System.Drawing.Size(180, 22);
+            this.lines.Text = "Lines";
+            this.lines.Click += new System.EventHandler(this.lines_Click);
+            // 
+            // linesStrip
+            // 
+            this.linesStrip.Name = "linesStrip";
+            this.linesStrip.Size = new System.Drawing.Size(180, 22);
+            this.linesStrip.Text = "Strip";
+            this.linesStrip.Click += new System.EventHandler(this.lineStrip_Click);
+            // 
+            // linesLoop
+            // 
+            this.linesLoop.Name = "linesLoop";
+            this.linesLoop.Size = new System.Drawing.Size(180, 22);
+            this.linesLoop.Text = "Loop";
+            this.linesLoop.Click += new System.EventHandler(this.lineLoop_Click);
+            // 
+            // trianglesP
+            // 
+            this.trianglesP.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.triangles,
+            this.trianglesFan,
+            this.trianglesStrip});
+            this.trianglesP.Name = "trianglesP";
+            this.trianglesP.Size = new System.Drawing.Size(180, 22);
+            this.trianglesP.Text = "Triangles";
+            // 
+            // triangles
+            // 
+            this.triangles.Name = "triangles";
+            this.triangles.Size = new System.Drawing.Size(180, 22);
+            this.triangles.Text = "Triangles";
+            this.triangles.Click += new System.EventHandler(this.triangles_Click);
+            // 
+            // trianglesFan
+            // 
+            this.trianglesFan.Name = "trianglesFan";
+            this.trianglesFan.Size = new System.Drawing.Size(180, 22);
+            this.trianglesFan.Text = "Fan";
+            this.trianglesFan.Click += new System.EventHandler(this.trianglesFan_Click);
+            // 
+            // trianglesStrip
+            // 
+            this.trianglesStrip.Name = "trianglesStrip";
+            this.trianglesStrip.Size = new System.Drawing.Size(180, 22);
+            this.trianglesStrip.Text = "Strip";
+            this.trianglesStrip.Click += new System.EventHandler(this.triangleStrip_Click);
+            // 
+            // quads
+            // 
+            this.quads.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quadsToolStripMenuItem,
+            this.quadStripToolStripMenuItem});
+            this.quads.Name = "quads";
+            this.quads.Size = new System.Drawing.Size(180, 22);
+            this.quads.Text = "Quads";
+            // 
+            // quadsToolStripMenuItem
+            // 
+            this.quadsToolStripMenuItem.Name = "quadsToolStripMenuItem";
+            this.quadsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quadsToolStripMenuItem.Text = "Quads";
+            this.quadsToolStripMenuItem.Click += new System.EventHandler(this.quadsToolStripMenuItem_Click);
+            // 
+            // quadStripToolStripMenuItem
+            // 
+            this.quadStripToolStripMenuItem.Name = "quadStripToolStripMenuItem";
+            this.quadStripToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quadStripToolStripMenuItem.Text = "QuadStrip";
+            this.quadStripToolStripMenuItem.Click += new System.EventHandler(this.quadStripToolStripMenuItem_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // polygon
+            // 
+            this.polygon.Name = "polygon";
+            this.polygon.Size = new System.Drawing.Size(180, 22);
+            this.polygon.Text = "Polygon";
+            this.polygon.Click += new System.EventHandler(this.polygon_Click);
+            // 
+            // task
+            // 
+            this.task.Name = "task";
+            this.task.Size = new System.Drawing.Size(180, 22);
+            this.task.Text = "Task";
+            this.task.Click += new System.EventHandler(this.task_Click);
             // 
             // OpenGLForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1053, 713);
+            this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.mHolst);
             this.Name = "OpenGLForm";
             this.Text = "OpenGLForm";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,6 +211,22 @@ namespace OpenGL
         #endregion
 
         private Tao.Platform.Windows.SimpleOpenGlControl mHolst;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pointsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quads;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ToolStripMenuItem linesP;
+        private System.Windows.Forms.ToolStripMenuItem lines;
+        private System.Windows.Forms.ToolStripMenuItem linesStrip;
+        private System.Windows.Forms.ToolStripMenuItem linesLoop;
+        private System.Windows.Forms.ToolStripMenuItem trianglesP;
+        private System.Windows.Forms.ToolStripMenuItem triangles;
+        private System.Windows.Forms.ToolStripMenuItem trianglesStrip;
+        private System.Windows.Forms.ToolStripMenuItem trianglesFan;
+        private System.Windows.Forms.ToolStripMenuItem quadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quadStripToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polygon;
+        private System.Windows.Forms.ToolStripMenuItem task;
     }
 }
 
--- OpenGL/OpenGLForm.cs
+++ OpenGL/OpenGLForm.cs
@@ -23,29 +23,317 @@ namespace OpenGL
             mHolst.InitializeContexts();
         }
 
-        private void mHolst_Paint(object sender, PaintEventArgs e)
+        private void pointsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Gl.glViewport(0,0, mHolst.Width, mHolst.Height);
+            Clear();
+            Gl.glEnable(Gl.GL_POINT_SMOOTH);
+            Gl.glPointSize(10);
+            Gl.glColor3f(0, 1, 1);
+
+            Gl.glBegin(Gl.GL_POINTS);
+            {
+                Gl.glVertex2f(ToCoordPoints(1), 0);
+
+                Gl.glVertex2f(ToCoordPoints(-1), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(-1), ToCoordPoints(-3));
+
+
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(1));
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(-1));
+
+
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(-3));
+
+            }
+            Gl.glEnd();
+
+            mHolst.Invalidate();
+        }
+        
+        private void Clear()
+        {
+            Gl.glViewport(0, 0, mHolst.Width, mHolst.Height);
             Gl.glClearColor(0.0f, 0.0f, 0.0f, 1);
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+        }
 
-            Gl.glEnable(Gl.GL_POLYGON_SMOOTH);
-            Gl.glEnable(Gl.GL_LINE_SMOOTH);
+        private void DrawTriangle(int mode)
+        {
+        }
 
+        private float ToCoordPoints(float points)
+        {
+            return 0.1f * points;
+        }
+
+        private void lines_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+
+            Gl.glColor3f(0, 1, 1);
+
+            Gl.glBegin(Gl.GL_LINES);
+            {
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(-3));
+
+
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(-3));
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(3));
+
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(3));
+
+
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(-3));
+
+
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(-3));
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(-3));
+
+            }
+            Gl.glEnd();
+
+            mHolst.Invalidate();
+        }
+
+        private void lineStrip_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+
+            Gl.glColor3f(0, 1, 1);
+
+            // Ромб
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            {
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(0));
+                Gl.glVertex2f(ToCoordPoints(0), ToCoordPoints(4));
+
+                
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(0));
+                
+                Gl.glVertex2f(ToCoordPoints(0), ToCoordPoints(-4));
+
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(0));
+            }
+            Gl.glEnd();
+
+            // куб
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            {
+                Gl.glVertex2f(ToCoordPoints(-4), ToCoordPoints(5));
+                Gl.glVertex2f(ToCoordPoints(4), ToCoordPoints(5));
+                
+                Gl.glVertex2f(ToCoordPoints(4), ToCoordPoints(-5));
+                
+                Gl.glVertex2f(ToCoordPoints(-4), ToCoordPoints(-5));
+
+                Gl.glVertex2f(ToCoordPoints(-4), ToCoordPoints(5));
+            }
+            Gl.glEnd();
+
+            mHolst.Invalidate();
+        }
+
+        private void lineLoop_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+
+            Gl.glColor3f(0, 1, 1);
+
+            // Ромб
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            {
+                Gl.glVertex2f(ToCoordPoints(0), ToCoordPoints(0));
+                Gl.glVertex2f(ToCoordPoints(2), ToCoordPoints(2));
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(3));
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(-3));
+                Gl.glVertex2f(ToCoordPoints(2), ToCoordPoints(-2));
+            }
+            Gl.glEnd();
+
+            // куб
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            {
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(4));
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(4));
+
+                Gl.glVertex2f(ToCoordPoints(3), ToCoordPoints(-4));
+
+                Gl.glVertex2f(ToCoordPoints(-3), ToCoordPoints(-4));
+            }
+            Gl.glEnd();
+
+            mHolst.Invalidate();
+        }
+
+        private void triangles_Click(object sender, EventArgs e)
+        {
+            Clear();
             Gl.glLineWidth(5);
             Gl.glBegin(Gl.GL_TRIANGLES);
             {
-                Gl.glColor3f(0, 0, 0);
-                Gl.glVertex2f(-0.5f, 0.5f);
-
                 Gl.glColor3f(0, 0, 1);
                 Gl.glVertex2f(0.5f, 0.5f);
 
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(-0.5f, 0.5f);
+
                 Gl.glColor3f(0, 1, 1);
                 Gl.glVertex2f(0, -0.5f);
+
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+        }
+
+        private void triangleStrip_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+            Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
+            {
+                Gl.glColor3f(0, 0, 1);
+                Gl.glVertex2f(-0.5f, 00);
+
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(0, -0.7f);
+
+                Gl.glColor3f(0, 1, 1);
+                Gl.glVertex2f(0, 0.7f);
                 
                 Gl.glColor3f(1, 1, 1);
+                Gl.glVertex2f(0.5f, 0);
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+
+        }
+
+        private void trianglesFan_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            {
+                Gl.glColor3f(0, 0, 1);
+                Gl.glVertex2f(0, 0);
+
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(0.5f, 0.5f);
+
+                Gl.glColor3f(0, 1, 1);
                 Gl.glVertex2f(-0.5f, 0.5f);
+                
+                Gl.glColor3f(1, 1, 1);
+                Gl.glVertex2f(0.5f, -0.5f);
+
+                Gl.glColor3f(1, 0, 1);
+                Gl.glVertex2f(-0.5f, -0.5f);
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+        }
+
+        private void quadsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+            Gl.glBegin(Gl.GL_QUADS);
+            {
+                Gl.glColor3f(0, 0, 1);
+                Gl.glVertex2f(-0.5f, 0.5f);
+
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(-0.5f, -0.5f);
+
+                Gl.glColor3f(0, 1, 1);
+                Gl.glVertex2f(0.5f, -0.5f);
+
+                Gl.glColor3f(1, 1, 1);
+                Gl.glVertex2f(0.5f, 0.5f);
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+        }
+
+        private void quadStripToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+            Gl.glBegin(Gl.GL_QUAD_STRIP);
+            {
+                Gl.glColor3f(1, 0, 0);
+                Gl.glVertex2f(-0.5f, 0.5f);
+
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(-0.5f, -0.5f);
+
+                Gl.glColor3f(1, 1, 1);
+                Gl.glVertex2f(0.5f, 0.5f);
+
+                Gl.glColor3f(1, 1, 0);
+                Gl.glVertex2f(0.5f, -0.3f);
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+        }
+
+        private void polygon_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+            Gl.glBegin(Gl.GL_POLYGON);
+            {
+                Gl.glColor3f(1, 0, 0);
+                Gl.glVertex2f(-0.3f, 0.5f);
+
+                Gl.glColor3f(0, 0.5f, 0);
+                Gl.glVertex2f(0.3f, 0.5f);
+
+                Gl.glColor3f(0, 0.5f, 1);
+                Gl.glVertex2f(0.6f, 0);
+
+                Gl.glColor3f(1, 0, 0.5f);
+                Gl.glVertex2f(0.3f, -0.5f);
+                
+                Gl.glColor3f(0, 0, 0.5f);
+                Gl.glVertex2f(-0.3f, -0.5f);
+                
+                Gl.glColor3f(0, 0.5f, 0);
+                
+                Gl.glVertex2f(-0.6f, 0);
+            }
+            Gl.glEnd();
+            mHolst.Invalidate();
+        }
+
+        private void task_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Gl.glLineWidth(5);
+
+            Gl.glColor3f(0, 1, 1);
+            
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            {
+                Gl.glVertex2f(ToCoordPoints(2), ToCoordPoints(-4));
+                Gl.glVertex2f(ToCoordPoints(2), ToCoordPoints(4));
+                Gl.glVertex2f(ToCoordPoints(0), ToCoordPoints(2));
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(4));
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(-2));
+            }
+            Gl.glEnd();
+            
+            Gl.glBegin(Gl.GL_LINES);
+            {
+                Gl.glVertex2f(ToCoordPoints(-2), ToCoordPoints(2));
+                Gl.glVertex2f(ToCoordPoints(2), ToCoordPoints(2));
             }
             Gl.glEnd();
 
